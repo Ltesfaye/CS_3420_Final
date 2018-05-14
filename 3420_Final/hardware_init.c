45df07ce71ac553c899ddf7f758a0d6f37bcf8c8
Ayush Mittal <ayushm23@gmail.com>
2018-05-14 16:58:01 -0400

    Got accelerometer code from Lab 6 handout.
    hardware_init should be a built-in file that needs to be modified to look like this file.

---
+++ 3420_Final/hardware_init.c
@@ -0,0 +1,12 @@
+void hardware_init(void) {
+
+	/* enable clock for ports */
+		
+	CLOCK_SYS_EnablePortClock(PORTA_IDX);
+	CLOCK_SYS-EnablePortClock(PORTB_IDX);
+	CLOCK_SYS_EnablePortClock(PORTE_IDX);
+
+	/* Init board clock */
+	BOARD_ClockInit();
+	dbg_uart_init();
+}
