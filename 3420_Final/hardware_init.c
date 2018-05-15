ca0ebfa0deebd8c9f7e856b87edbc9427bfdaf97
Ayush Mittal <ayushm23@gmail.com>
2018-05-15 15:51:08 -0400

    Update hardware_init.c

--- 3420_Final/hardware_init.c
+++ 3420_Final/hardware_init.c
@@ -3,7 +3,7 @@ void hardware_init(void) {
 	/* enable clock for ports */
 		
 	CLOCK_SYS_EnablePortClock(PORTA_IDX);
-	CLOCK_SYS-EnablePortClock(PORTB_IDX);
+	CLOCK_SYS_EnablePortClock(PORTB_IDX);
 	CLOCK_SYS_EnablePortClock(PORTE_IDX);
 
 	/* Init board clock */
